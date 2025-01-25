e40dfa81252476f3e4dfec2c61a32e50d8aeb3cd
Aniss Aziz <anissaziz31000@gmail.com>
2025-01-25 11:24:24 +0100

    feat: add feature to get all users

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -3,6 +3,8 @@ import { MongoClient } from "mongodb";
 const uri = 'mongodb+srv://airbnbclone:airbnbclone@cluster.7xjgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster';
 const client = new MongoClient(uri);
 
+
+// Create USER
 export async function POST(req) {
   try {
     const data = await req.json(); // Cette méthode permet d'obtenir le body de la requête dans Next.js 13+
@@ -57,6 +59,43 @@ export async function POST(req) {
     console.error("Error during registration:", error); // Log de l'erreur
     return new Response(
       JSON.stringify({ message: "Error registering user", error: error.message }),
+      { status: 500 }
+    );
+  } finally {
+    await client.close();
+  }
+}
+
+// GET USER
+
+export async function GET() {
+  try {
+    console.log("Connecting to database...");
+    await client.connect(); // Connexion à la base de données
+    
+    const db = client.db("airbnbclone"); // Nom de votre base de données
+    const collection = db.collection("users");
+
+    // Récupérer tous les utilisateurs inscrits
+    const users = await collection.find({}).toArray();
+    
+    // Si aucun utilisateur trouvé
+    if (users.length === 0) {
+      return new Response(
+        JSON.stringify({ message: "No users found" }),
+        { status: 404 }
+      );
+    }
+
+    console.log("Users fetched successfully");
+    return new Response(
+      JSON.stringify({ users }), // Retourner la liste des utilisateurs
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error fetching users:", error); // Log de l'erreur
+    return new Response(
+      JSON.stringify({ message: "Error fetching users", error: error.message }),
       { status: 500 }
     );
   } finally {
